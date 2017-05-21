c29734c64587417c625e372bf82ba5f88486bb82
norbertgogibiro <norbi.mme@hotmail.com>
2017-05-21 13:41:18 +0200

    Polishing structure

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<?php require_once('templates/head.php'); ?>
+	<body>
+
+
+
+		<!--JQUERY LINKED IN BELOW-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
---
+++ templates/head.php
@@ -0,0 +1,4 @@
+<head>
+	<title>Under Discussion</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
