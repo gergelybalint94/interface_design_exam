6c624d2a4b8b44400a3a9d7fed70834bbb2dc383
norbertgogibiro <norbi.mme@hotmail.com>
2017-05-21 22:18:09 +0200

    Homepage, header and sidebar done

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<header>
+	<div class="content-max-width-wide" id="header-bar-content">
+
+		<!-- SANDWICH MENU ICON: -->
+		<div class="header-elements" id="sandwich-icon-wrap">
+			<div class="links-active fa fa-bars fa-fw" id="sandwich-icon"></div>
+		</div>
+
+		<!-- HEADER LOGO: -->
+		<div class="header-elements links-active" id="header-logo"></div>
+
+		<!-- SHORTCUT ICONS: -->
+		<div class="header-elements" id="header-shortcuts-wrap">
+			<div class="header-shortcut-icons links-inactive fa fa-bell-o fa-fw" id="search-icon"></div>
+			<div class="header-shortcut-icons links-inactive fa fa-search fa-fw" id="bell-icon"></div>
+			<div class="header-shortcut-icons links-active fa fa-user-circle fa-fw" id="account-icon"></div>
+		</div>
+	</div>
+</header>
