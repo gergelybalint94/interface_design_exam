1fc6ee9e5a07cc89224de34962298f66a75b2ec6
gergelybalint94 <gergelybalint94@gmail.com>
2017-05-22 17:46:03 +0200

    Fully working registration with ajax

--- js/script.js
+++ js/script.js
@@ -20,3 +20,34 @@ $(document).on('click', '.closer-elements', function(){
 	.siblings('.destroyer-backgrounds:first-of-type')
 	.fadeOut();
 });
+
+//Function for the popping up signup box
+$(document).on("click", "#account-icon", function(){
+	console.log("works")
+	$("#registration-area").css("display", "flex");
+});
+
+// Function to save a new user
+function createUser(firstName, lastName, password, email ){
+	var sFirstName = firstName;
+	var sLastName = lastName;
+	var sPassword = password;
+	var sEmail = email;
+	$.ajax({
+		"method":"POST",
+		"url":"api/api-create-user.php",
+		"dataType":"json",
+		"data":{"firstName":sFirstName,"lastName":sLastName,"password":sPassword, "email":sEmail},
+	}).done(function(sData){
+		$("#registration-area").css("display", "none");
+});
+};
+
+//Call the createUser function
+$("#submit-registration").on("click",function(){
+	var sFirstName = $("#firstName").val();
+	var sLastName = $("#lastName").val();
+	var sPassword = $("#password").val();
+	var sEmail = $("#email").val();
+	createUser(sFirstName, sLastName, sPassword, sEmail);
+});
