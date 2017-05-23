689a08fd856782c93e5dfbebd3897641e2ffa1a3
norbertgogibiro <norbi.mme@hotmail.com>
2017-05-23 16:13:33 +0200

    partners' page progress 30%

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES:
 // ██████████████████████████████
 
-var sActualPage	= document.location.href.match(/[^\/]+$/)[0];
+var sActualPage	= ( document.location.href.match(/[^\/]+$/) !== null ) ? document.location.href.match(/[^\/]+$/)[0] : 'index.php';
 
 
 
@@ -26,52 +26,151 @@ switch( sActualPage ){
 				'label'		: 'add new event',
 				'icon'		: 'add_event.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'manage event list',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'add new partner',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'manage partner list',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'manage user list',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'add new website admin',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'edit site contents',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			},
 			{
 				'label'		: 'general site settings',
 				'icon'		: 'edit_partners.svg',
 				'active'	: false,
+				'highlight'	: false,
 				'href'		: ''
 			}
 		];
 		fnPopulateGridArea('#control-panel-grids', ajGridData);
+		break;
+
+	case 'partners.php':
+		var ajGridData = [
+			{
+				'label'		: 'ibm',
+				'icon'		: 'add_event.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'danske bank',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'google',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'microsoft',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'magnetix',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'peytz&co',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'ideo',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'danskbiotek',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'novo nordisk',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'blue ocean robotics',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'transfer wise',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: false,
+				'href'		: ''
+			},
+			{
+				'label'		: 'contact us',
+				'icon'		: 'edit_partners.svg',
+				'active'	: false,
+				'highlight'	: true,
+				'extraLine'	: 'become our new partner',
+				'href'		: ''
+			}
+		];
+		fnPopulateGridArea('#partners-grids', ajGridData);
 		break;
 }
 
@@ -103,18 +202,21 @@ $(document).on('click', '[data-href]', function(){
 // Function to populate grid areas:
 function fnPopulateGridArea( sGridAreaSelector, ajGridData ){
 	var sBluePrint = 
-		'<div class="grids {{is-active}}" data-href="{{href}}">' +
+		'<div class="grids {{is-active}} {{is-highlighted}}" data-href="{{href}}">' +
 			'<div class="grid-icons" style="background-image: url(&quot;assets/icons/{{icon}}&quot;);"></div>' +
+			'{{is-extra-line}}' +
 			'<div class="grid-labels">{{label}}</div>' +
 		'</div>';
 
 	for(var i = 0; i < ajGridData.length; i++){
 
 		var sBluePrintCopy = sBluePrint;
-			sBluePrintCopy = sBluePrintCopy.replace('{{is-active}}', ( ajGridData[i].active === true ) ? 'links-active' : 'links-inactive' );
-			sBluePrintCopy = sBluePrintCopy.replace('{{href}}',  ajGridData[i].href);
-			sBluePrintCopy = sBluePrintCopy.replace('{{icon}}',  ajGridData[i].icon);
-			sBluePrintCopy = sBluePrintCopy.replace('{{label}}',  ajGridData[i].label);
+			sBluePrintCopy = sBluePrintCopy.replace('{{is-active}}', ( typeof( ajGridData[i].active ) === 'undefined' || ajGridData[i].active === false ) ? 'links-inactive' : 'links-active' );
+			sBluePrintCopy = sBluePrintCopy.replace('{{is-highlighted}}', ( typeof( ajGridData[i].active ) === 'undefined' || ajGridData[i].highlight === false ) ? '' : 'highlighted' );
+			sBluePrintCopy = sBluePrintCopy.replace('{{href}}', ajGridData[i].href);
+			sBluePrintCopy = sBluePrintCopy.replace('{{icon}}', ajGridData[i].icon);
+			sBluePrintCopy = sBluePrintCopy.replace('{{label}}', ajGridData[i].label);
+			sBluePrintCopy = sBluePrintCopy.replace('{{is-extra-line}}', ( typeof( ajGridData[i].extraLine ) === 'undefined' || ajGridData[i].extraLine === false ) ? '' : '<p class="extra-lines">' + ajGridData[i].extraLine + '</p>' );
 		$( sGridAreaSelector ).append( sBluePrintCopy );
 	}
 }
