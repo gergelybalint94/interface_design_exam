4044161e8f28bd423eafc358383b653b51e1599a
norbertgogibiro <norbi.mme@hotmail.com>
2017-05-25 22:36:08 +0200

    on page load SWAL deleted

--- js/script.js
+++ js/script.js
@@ -6,21 +6,6 @@ var bLoggedIn = false;
 
 
 
-swal({
-  title: "Are you sure?",
-  text: "You will not be able to recover this imaginary file!",
-  type: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  confirmButtonText: "Yes, delete it!",
-  closeOnConfirm: false
-},
-function(){
-  swal("Deleted!", "Your imaginary file has been deleted.", "success");
-});
-
-
-
 
 
 
